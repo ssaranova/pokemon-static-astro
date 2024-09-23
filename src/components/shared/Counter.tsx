3c0b71b73c6682e1286bc50f425c1a83dcf43177
Saúl Saranova <ssaranovas@gmail.com>
2024-09-23 19:14:40 +0200

    Show element inside island component as React children

--- src/components/shared/Counter.tsx
+++ src/components/shared/Counter.tsx
@@ -1,7 +1,8 @@
-import { createSignal, type Component } from "solid-js";
+import { createSignal, type Component, type JSX } from "solid-js";
 
 interface Props {
     initValue: number;
+    children?: JSX.Element
 }
 
 export const Counter: Component<Props> = (props) => {
@@ -9,7 +10,7 @@ export const Counter: Component<Props> = (props) => {
 
     return (
         <>
-            <h1 class="text-4xl">Counter</h1>
+            {props.children}
             <h3 class="text-xl">Value: {counter()}</h3>
 
             <button class="bg-blue-500 p-2 mr-2 rounded" onclick={() => setCounter((prev) => ++prev)}>+1</button>
